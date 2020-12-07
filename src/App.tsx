64512168860e2e8e590103607e1eb93a83be307e
Christopher Kodama <ckhordiasma@gmail.com>
2020-12-06 21:26:42 -0500

    moved threejs stuff into separate component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import * as THREE from "three";
+import ThreeScene from './components/ThreeScene'
+
 
 function App() {
-  const mountPoint = React.useRef(null);
-  React.useEffect(()=>{
-    // === THREE.JS CODE START ===
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    
-    mountPoint.current.appendChild( renderer.domElement );
 
-    const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    const cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
-    camera.position.z = 5;
-    const animate = function () {
-      requestAnimationFrame( animate );
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-      renderer.render( scene, camera );
-    };
-    animate();
-    // === THREE.JS EXAMPLE CODE END ===
-  },[])
   return (
-    <div className="App" ref={mountPoint}/>
+    <>
+
+    <ThreeScene/>
+    </>
   );
 }
 
