5f733e9ec15afd399d68b1259dcac53eacff3692
Christopher Kodama <ckhordiasma@gmail.com>
2020-12-07 01:19:10 -0500

    added more comments in code

--- src/components/ThreeScene.tsx
+++ src/components/ThreeScene.tsx
@@ -14,27 +14,72 @@ function ThreeScene({ speed = 0.01 }) {
     const geometry = new THREE.BoxGeometry(1, 1, 1);
     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
     
-    const [sceneObjects, setSceneObjects] = React.useState([]);
+    const [sceneObjects, setSceneObjects] = React.useState(new Map());
     
-    
-    const setupScene = () => {
+    // initial setup of scene, camera, and renderer when component mounts.
+    React.useEffect(() => {
+        
+        const rendererInit = new THREE.WebGLRenderer();
+        rendererInit.setSize(window.innerWidth, window.innerHeight);
+        if (mountPoint.current !== null) mountPoint.current.appendChild(rendererInit.domElement);
+        const sceneInit = new THREE.Scene();
+        const cameraInit = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        
+        // DO all setup related things here //
+
+        // manage the state of the scene, camera, and renderer, which allows for them to be accessible in other
+        // hooks and parts of this component function, like setupSceneObjects() below
+        setScene(sceneInit);
+        setCamera(cameraInit);
+        setRenderer(rendererInit);
+
+        // for when the component unmounts
+        return ()=>{
+            stop();
+        }
+    }, [])
+
+
+    // This hook handles inserting objects into the scene once the scene is generated.
+    // Note that had to guard here to avoid issues with this being called on initial mount when everything is undefined
+    React.useEffect(()=>{
+        if(scene != null){
+            setupSceneObjects()
+        }
+    }, [scene])
+
+    // Note that here, in addition to adding the cube to the scene, I'm adding it to a map called sceneObjects.
+    //   sceneObjects is defined in the top as a React State, and can be referenced in the animation loop
+    //    to make changes to specific
+    const setupSceneObjects = () => {
         const cube = new THREE.Mesh(geometry, material);
-        setSceneObjects((old) => [...old,cube]);
+        setSceneObjects(sceneObjects.set('cube1', cube));
         scene.add(cube);        
         camera.position.z = 5;
     }
 
-    const renderScene = () => {
-        sceneObjects.forEach((obj)=>{
-            obj.rotation.x += speed;
-            obj.rotation.y += speed;
-        })
+    // When props (i.e. speed) change from the parent component, we want the variables within animate() to be updated accordingly. 
+    //  This can be done by stopping and reinitializing animate().
+    // we also want this to happen when objects in the scene are added or removed
+    React.useEffect(()=>{
+        if(scene != null){
+            stop(); start();
+        }
+    },[speed, scene, sceneObjects])
+
+    // This function runs when animate() is called. 
+    // All objects intended for update should be managed with React.useState() and 
+    //   should be included as dependencies in the hook above.
+    const updateScene = () => {
+        const cube = sceneObjects.get('cube1');
+        cube.rotation.x += speed;
+        cube.rotation.y += speed;
         
     };
 
     const animate = () => {
         frameId.current = (window.requestAnimationFrame(animate)); 
-        renderScene();
+        updateScene();
         renderer.render(scene, camera);
     }
 
@@ -45,46 +90,6 @@ function ThreeScene({ speed = 0.01 }) {
         cancelAnimationFrame(frameId.current);
         frameId.current = 0;
     }
-
-    const restart = ()=>{stop();start();}
-
-    // initial setup of scene, camera, and renderer when component mounts.
-    React.useEffect(() => {
-        setScene(() => new THREE.Scene() );
-        setCamera( () => new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000) )
-        setRenderer(() => new THREE.WebGLRenderer());
-
-        return ()=>{
-            stop();
-        }
-    }, [])
-
-    // configuring renderer. Note that I had to guard with renderer != null because this runs once on mount 
-    // (renderer is not defined then) and again on subsequent changes to renderer
-    React.useEffect(()=>{
-        if(renderer != null) {
-            renderer.setSize(window.innerWidth, window.innerHeight);
-            if (mountPoint.current !== null) mountPoint.current.appendChild(renderer.domElement);
-        }
-    },[renderer])
-
-    // setting up the scene with objects and stuff. Note that had to guard here as well to avoid issues with 
-    //   this being called on initial mount when everything is undefined
-    React.useEffect(()=>{
-        if(scene != null){
-            setupScene();
-        }
-    }, [scene])
-
-    // When props (i.e. speed) change from the parent component, we want the variables within animate() to be updated accordingly. 
-    //  This can be done by stopping and reinitializing animate().
-    // we also want this to happen when objects in the scene are added or removed
-    React.useEffect(()=>{
-        if(scene != null){
-            restart();
-        }
-    },[speed, scene, sceneObjects])
-
 
     return (
         <div ref={mountPoint} />
